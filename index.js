a3d57b14c2fde5c4bbe0282bc62164945b564ac3
Michael Bernasol <mfbernasol@gmail.com>
2020-09-25 14:31:50 -0500

    add dice images and randomize on refresh

---
+++ index.js
@@ -0,0 +1,8 @@
+var randomInt = Math.floor(Math.random()* 6) + 1;
+var randomInt2 = Math.floor(Math.random()* 6) + 1;
+
+var diceImage = "images/dice" + randomInt + ".png";
+var diceImage2 = "images/dice" + randomInt2 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",diceImage);
+document.querySelectorAll("img")[1].setAttribute("src",diceImage2);
