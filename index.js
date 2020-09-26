48fb79639bbdfef7b51f79dc8ec86ebb9ab5573d
Michael Bernasol <mfbernasol@gmail.com>
2020-09-26 12:58:46 -0500

    add instructions, add autofocus to button

--- index.js
+++ index.js
@@ -17,4 +17,14 @@ function rollDice(){
 
     document.querySelectorAll("img")[0].setAttribute("src",diceImage);
     document.querySelectorAll("img")[1].setAttribute("src",diceImage2);
+
+    if(randomInt > randomInt2){
+        document.querySelector("h1").innerHTML = "Player 1 Wins!"
+    }else if(randomInt2 > randomInt){
+        document.querySelector("h1").innerHTML = "Player 2 Wins!"
+    }else {
+        document.querySelector("h1").innerHTML = "It's a Draw."
+    }
 }
+
+
